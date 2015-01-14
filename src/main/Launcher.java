7768654865e533e873f41b36fa1e685ace4c516a
Caroline C <caroline.chea90@gmail.com>
2015-01-14 16:52:36 +0100

    last

--- src/main/Launcher.java
+++ src/main/Launcher.java
@@ -1,13 +1,7 @@
 package main;
 import java.io.File;
 
-import org.apache.catalina.Context;
-import org.apache.catalina.core.AprLifecycleListener;
-import org.apache.catalina.core.StandardServer;
-import org.apache.catalina.loader.WebappLoader;
 import org.apache.catalina.startup.Tomcat;
-
-import servlet.Dispatcher;
 
 
 public class Launcher {
@@ -19,15 +13,17 @@ public class Launcher {
 		
 		String webPort = System.getenv("PORT");
 		if(webPort==null || webPort.isEmpty()){
-			webPort = "8080";
+			webPort = "8888";
 		}
 
 		tomcat.setPort(Integer.valueOf(webPort));
 
-		tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
+		tomcat.addWebapp("/gestion", new File(webappDirLocation).getAbsolutePath());
 		
 		System.out.println("Configuration de l'application avec baseDir"+new File(webappDirLocation).getAbsolutePath());
+
 		tomcat.start();
+
 		tomcat.getServer().await();
 	}
 
